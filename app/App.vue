378363b5b69a03a958a59dd1fa1247107580ab15
Pierre Beitz <grimsen205@googlemail.com>
2017-11-19 13:56:04 +0100

    add vue-google-autocomplete

--- app/App.vue
+++ app/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     hi
+    <vue-google-autocomplete> </vue-google-autocomplete>
   </div>
 </template>
 
--- app/main.js
+++ app/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './App';
 import 'vueify/lib/insert-css';
+import VueGoogleAutocomplete from 'vue-google-autocomplete';
 
 new Vue({
   el: '#app',
+  components: {
+    VueGoogleAutocomplete,
+  },
   render: h => h(App),
 });
